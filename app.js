c256e52b52dc72bc118d3711290d0047e542d0ee
Winn <winnieee.lam00@gmail.com>
2020-02-06 21:08:04 -0500

    created server in app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const port = process.env.PORT || 3000
+let fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  fs.readFile('./index.html', null, function(error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ` + port);
+});
